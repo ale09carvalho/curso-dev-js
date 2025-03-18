4447d32e01dc99c6cb9acfcbd90d148fd183bc6c
Alê <110263831+ale09carvalho@users.noreply.github.com>
2025-03-18 15:51:29 -0300

    Create script.js
    tipos de dados js

---
+++ tiposdados/script.js
@@ -0,0 +1,42 @@
+/* .console.log ("Hello word!!!!!!!!!!!!!!!!!!!!");
+*O método de console log() envia uma mensagem para o console do navegador (na aba Console)
+*
+* JavaScript tem 8 tipos de dados
+* String
+* Número
+* Bigint
+* Boolean
+* Indefinido
+* Nulo
+* Símbolo
+* Objeto
+==> pode conter objetos internos e objetos definidos pelo usuário :
+
+const d = 'a'  -- nao tem tipo char ou caracter e tudo tipo string mesmo com 1 so caractar
+const f = {name: 'Maria', age: 25} --- e um objeto começa com chaves
+onst g = ['a', 15, false, 'maria'] --- e um array um objtos
+const i = undefined -- variavel nao definida
+
+const a = 10
+console.log(typeof a) --- imprime el tipo de dato de la variable
+
+*/
+const a = 10
+const b = 10.5
+const c = 'Maria'
+const d = 'a'
+const e = false
+const f = {name: 'Maria', age: 25}
+const g = ['a', 15, false, 'maria']
+const h = null
+const i = undefined
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+console.log(typeof h)
+console.log(typeof i)
